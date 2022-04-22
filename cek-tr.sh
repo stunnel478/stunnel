1dc6d6dc3939aeebb9a987f300423140b4c6abe7
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-04-22 19:31:50 +0700

    Update cek-tr.sh

--- cek-tr.sh
+++ cek-tr.sh
@@ -4,14 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl http://vip-sg1.myvpskuy.xyz:81/BskaoahdmsoahamoaJNlapabsmal | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 data=( `cat /var/log/trojan.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
 echo "-------------------------------";
 echo "-----=[ Trojan User Login ]=-----";
