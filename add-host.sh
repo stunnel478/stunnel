3592152e9633a0d25da0b3aee5adcff49ade02dd
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-04-22 21:46:56 +0700

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -18,13 +18,17 @@ echo "Checking VPS"
 clear
 read -rp "Domain/Host : " -e domain
 echo "IP=$domain" >>/var/lib/premium-script/ipvps.conf
-echo $domain > /etc/v2ray/domain
-echo start
-systemctl stop v2ray.service
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
-systemctl start v2ray.service
-echo Done
-sleep 1.5
-clear
-neofetch
+rm -rf /etc/xray/domain
+echo $domain > /etc/xray/domain
+
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+restart
