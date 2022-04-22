0df70c71daa6f4c08907bdd9b5b0eb129f773258
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-04-22 19:58:33 +0700

    Create add-host.sh

---
+++ add-host.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+clear
+read -rp "Domain/Host : " -e domain
+echo "IP=$domain" >>/var/lib/premium-script/ipvps.conf
+echo $domain > /etc/v2ray/domain
+echo start
+systemctl stop v2ray.service
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl start v2ray.service
+echo Done
+sleep 1.5
+clear
+neofetch
