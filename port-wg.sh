11b8abc4a0d5083888fb23332ef5bd27f8290669
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-04-22 19:44:31 +0700

    Update port-wg.sh

--- port-wg.sh
+++ port-wg.sh
@@ -5,14 +5,6 @@ NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 NIC=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 echo "Checking VPS"
-IZIN=$( curl http://vip-sg1.myvpskuy.xyz:81/BskaoahdmsoahamoaJNlapabsmal | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 wg="$(cat ~/log-install.txt | grep -i Wireguard | cut -d: -f2|sed 's/ //g')"
 echo -e "      Change Port $wg"
