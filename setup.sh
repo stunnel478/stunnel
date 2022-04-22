aafea601dd38b4cd04e94e3735df4610c699036d
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-04-22 19:47:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,37 +11,26 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl http://link kalian:81/BskaoahdmsoahamoaJNlapabsmal | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-echo "Telegram t.me/horasss"
-echo "WhatsApp wa.me/6282381643124"
-rm -f setup.sh
-exit 0
-fi
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://linkkalian/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://linkkalian/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://linkkalian/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-wget https://linkkalian/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://linkkalian/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget hhttps://raw.githubusercontent.com/stunnel478/stunnel/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-wget https://linkkalian/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget http://prem.vpnstores.net/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget hhttps://raw.githubusercontent.com/stunnel478/stunnel/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-wget https://linkkalian/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-wget https://linkkalian/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -66,7 +55,7 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://linkkalian/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/stunnel478/stunnel/main/set.sh"
 chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
