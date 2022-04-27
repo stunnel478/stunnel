dcd0b3f66d5d05808f28bd69a1f5cbe8f5d7d792
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-04-27 13:43:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/stunnel478/stunnel/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/cff.sh && chmod +x cff.sh && ./cff.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/stunnel478/stunnel/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
