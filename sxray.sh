49e82852d7e1aa82958a51bc4a352d89571645b2
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-05-16 09:51:09 +0700

    Create sxray.sh

---
+++ sxray.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+if [ -f "/etc/v2ray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/cff.sh && chmod +x cff.sh && ./cff.sh
+#install ssh ovpn
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+#install ssr
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+#installwg
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+#install v2ray
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+#install L2TP
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/stunnel478/stunnel/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+
+rm -f /root/ssh-vpn.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/ss.sh
+rm -f /root/ssr.sh
+rm -f /root/ins-vt.sh
+rm -f /root/ipsec.sh
+rm -f /root/set-br.sh
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=https://vpnstores.net
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -O /etc/set.sh "https://raw.githubusercontent.com/stunnel478/stunnel/main/set.sh"
+chmod +x /etc/set.sh
+history -c
+echo "1.2" > /home/ver
+clear
+echo " "
+echo "Installation has been completed!!"
+echo ""
+echo " Reboot 15 Sec"
+sleep 15
+rm -f sxray.sh
+reboot
+
